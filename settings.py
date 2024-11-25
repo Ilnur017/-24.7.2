3c1927b68e6cc3f397198101fb8621b0eebf5762
Нургалиев Ильнур <ilnurnailevich17@gmail.com>
2024-11-25 17:30:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import os
 
-valid_email = "chuvachyok@rambler.ru"
-valid_password = "Y821RO02rus"
+valid_email = "ilnurnailevi4@mail.ru"
+valid_password = "123456"
 
-unvalid_email = "gus@vader.com"
-unvalid_password = "2255888yuegg"
+unvalid_email = "cola@gmail.com"
+unvalid_password = "123456"
 
 invalid_auth_key = {'key': '555c0c8710fd04e2f53c3637407c7b1080e9f40be53e41a84dc94637'}
